46a5a742260ef3d081a20765783aa6a513ab1e7c
Wrbrgrgl <alex@zobristfamily.com>
2016-07-17 15:16:22 -0700

    data protect yo

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 #!python
 #thisismyfirstpythonpogromitsays hello worlb
-
+peoples = ['Claire', 'Alex', 'Blake', 'Dad', 'Mom']
 #favorite stuff yall
 favorite_items = {'Claire':'Pokemon','Alex':'Magic Cards',
-                  'Blake':'His spot on the couch',}
+                  'Blake':'his spot on the couch',
+                  'Mom':'napping',
+                  }
 my_string = "{} really loves {}!"
 
-for person in favorite_items:
-    print(my_string.format(person, favorite_items[person]))
+for person in sorted(peoples):
+    if person in favorite_items.keys():
+        print(my_string.format(person, favorite_items[person]))
+    else:
+        print "Error, {} has no favorites".format(person)
