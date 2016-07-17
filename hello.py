b423835fefa1fca3127e16cb93475eb6b519a8e6
Wrbrgrgl <alex@zobristfamily.com>
2016-07-17 14:38:01 -0700

    XrGGdsa

--- hello.py
+++ hello.py
@@ -6,5 +6,5 @@ favorite_items = {'Claire':'Pokemon','Alex':'Magic Cards',
                   'Blake':'Osama Bin Laden',}
 my_string = "{} really loves {}!"
 
-for person in favorite_items
+for person in favorite_items:
     print(my_string.format(person, favorite_items[person]))
